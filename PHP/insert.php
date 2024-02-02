09c876e1ff0a68b1b0533042160f190974107970
Bilal Shahid <129677698+BilalShahid-13@users.noreply.github.com>
2024-02-02 19:51:19 +0500

    Update insert.php

--- PHP/insert.php
+++ PHP/insert.php
@@ -1,11 +1,17 @@
+//Firstly open your local server like xammpp then copy path like http://localhost/abc/insert.php abc is mine folder where my insert.php means this file is running in local server
+// if you have any query or porblems then do let me know
 <?php
 
 header('Access-Control-Allow-Origin');
 header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-
-$conn = mysqli_connect("localhost", "root", "", "abc");
+// connetion
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "abc";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     echo 'console.log(mysqli_connect_error)';
     exit;
@@ -13,7 +19,7 @@ if ($conn->connect_error) {
     if (isset($_POST['submit'])) {
         $name = $_POST['username'];
         $email = $D_POST['pass'];
-
+// reg is table
         $sql = "INSERT INTO reg VALUES ('','$name', '$email')";
 
         if ($conn->query($sql) === TRUE) {
